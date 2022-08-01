94150709dc805a391092c5de470c299cd36b2ff0
WTlumos <1543315941@qq.com>
2022-08-02 01:23:01 +0800

    改用mysql2

--- api-server/db/index.js
+++ api-server/db/index.js
@@ -1,14 +1,14 @@
 // 导入mysql模块
-const db = require("mysql");
+const db = require("mysql2");
 
 // 创建数据库连接
 const connection = db.createConnection({
-    host: "127.0.0.1",
+    host: "ip",
     user: "root",
     port: "3306",
     password: "密码",
     database: "my_db_01"
 })
-connection.connect();
+// connection.connect();
 // 向外共享 db 数据库连接对象
 module.exports = connection;
